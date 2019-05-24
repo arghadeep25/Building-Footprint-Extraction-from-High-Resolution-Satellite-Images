c4f6ca599c8aa17bd93fdda74bb753883eb6b2e9
Arghadeep <arghadeep25@gmail.com>
2019-05-24 21:16:16 +0200

    Added Inria Dataset Generator Class

--- utils/data_generator.py
+++ utils/data_generator.py
@@ -1,12 +1,13 @@
-""" Name: Utils
-    Author: Arghadeep Mazumder
-    Version: 0.1
-    Description: 1. File for generation of various data
-                    - Inria Dataset
-                    - DSAC Dataset
-                    - CrowdAI Building Dataset
-                    - Nucleus Dataset Kaggle
-                 2.
+"""
+Name: Utils
+Author: Arghadeep Mazumder
+Version: 0.1
+Description: 1. File for generation of various data
+                - Inria Dataset
+                - DSAC Dataset
+                - CrowdAI Building Dataset
+                - Nucleus Dataset Kaggle
+             2.
 """
 import numpy as np
 import sys
