9641cb03aba52a0d7c078f2e9c66eb10ded3fcaa
Arghadeep <arghadeep25@gmail.com>
2019-05-27 15:59:02 +0200

    tested train unet model

--- main.py
+++ main.py
@@ -3,19 +3,26 @@ import sys
 import time
 import cv2
 from utils import loader
+from models.unet import UNet
+from train.trainUNet import TrainUNet
 
 def main():
+    # path = sys.argv[1]
+    #
+    # data = loader.InriaDataLoader(path, rotation =90, aug = True)
+    # images, masks = data.__getitem__()
+    #
+    # i =30
+    # cv2.imshow('Image', images[i])
+    # cv2.imshow('Mask', masks[i])
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    # print(len(images))
     path = sys.argv[1]
 
-    data = loader.InriaDataLoader(path, rotation =90, aug = True)
-    images, masks = data.__getitem__()
-
-    i =30
-    cv2.imshow('Image', images[i])
-    cv2.imshow('Mask', masks[i])
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    print(len(images))
+    net = UNet(n_channels = 3, n_classes = 1)
+    train_unet = TrainUNet(data_path= path, net = net)
+    train_unet.train()
 
 if __name__ == '__main__':
     main()
