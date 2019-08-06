848fbef34bf8cd0c7f50c8e05689736b25462e51
Arghadeep Mazumder <arghadeep25@gmail.com>
2019-08-06 20:41:25 +0200

    small fix

--- activation_functions_plots/plot.py
+++ activation_functions_plots/plot.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 
 class GraphPlot():
-    def __init__(self, model, x_val, y_val, z_val):
+    def __init__(self, model,save_model, x_val, y_val, z_val):
         self.model = model
         self.x_val = x_val
         self.y_val = y_val
         self.z_val = z_val
+        self.save_model = save_model
 
     def plot(self):
         fig, ((ax1), (ax2)) = plt.subplots(1,2, figsize = (18,8))
@@ -52,6 +53,7 @@ class GraphPlot():
             ax2.set_xlim([-5,5])
 
         plt.subplots_adjust(bottom=0.15, wspace=0.5)
-        filename = 'plots/' + self.model + '.png'
-        plt.savefig(filename)
+        if self.save_model == True:
+            filename = 'plots/' + self.model + '.png'
+            plt.savefig(filename)
         plt.show()
