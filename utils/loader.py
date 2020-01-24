eb8ae9d5b16a10636435b65d41cdb06dfda64d13
arghadeep25 <arghadeep25@gmail.com>
2020-01-24 21:29:19 +0100

    updated

--- utils/loader.py
+++ utils/loader.py
@@ -3,10 +3,10 @@ Name: Data Loader
 Author: Arghadeep Mazumder
 Version: 0.1
 Description: 1. File for generation of various data
-                - Inria Dataset
-                - DSAC Dataset
+                - Inria Training Dataset
+                - Inria Test Dataset
                 - CrowdAI Building Dataset
-                - Nucleus Dataset Kaggle
+                - SpaceNet Buidling Dataset
              2.
 """
 import os
@@ -18,9 +18,6 @@ import numpy as np
 import keras.utils
 from .augmentation import DataAugmentation
 sys.path.append('../')
-# from models.mask_rcnn import utils
-# from models.mask_rcnn.config import Config
-# from models.mask_rcnn import model as modellib
 from keras.preprocessing.image import ImageDataGenerator, img_to_array
 
 
@@ -34,9 +31,10 @@ class InriaDataLoader(keras.utils.Sequence):
 
     """
     def __init__(self, data_ids, data_path, patch_size = 256,
-                batch_size = 8, aug = False, rotation = 0,
-                zoom_range = 1, horizontal_flip = False,
-                vertical_flip = False, shear = 0, split_channel = False):
+                batch_size = 8, aug = True, rotation = 90,
+                zoom_range = 1.5, horizontal_flip = True, hist_eq = True,
+                vertical_flip = True, shear = 0.2, brightness = True,
+                add_noise = True, sigma = 10, split_channel = False):
 
         self.data_path = data_path
         self.data_ids = data_ids
@@ -49,6 +47,10 @@ class InriaDataLoader(keras.utils.Sequence):
         self.vertical_flip = vertical_flip
         self.shear = shear
         self.split_channel = split_channel
+        self.hist_eq = hist_eq
+        self.brightness = brightness
+        self.add_noise = add_noise
+        self.sigma = sigma
 
     def __load__(self, data_name):
         """ Load an image and a mask from the data folder
@@ -58,24 +60,22 @@ class InriaDataLoader(keras.utils.Sequence):
         mask_name_path = os.path.join(self.data_path, 'gt', data_name)
 
         image = cv2.imread(image_name_path)
+        image = img_to_array(image)
 
         mask = cv2.imread(mask_name_path)
         mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
 
-        image = img_to_array(image)/255.
-        mask = mask/255.
-
-        split_mask = np.zeros((self.patch_size, self.patch_size, 2))
-
-        # Mainly for PSPNet
+        # PSPNet and SegNet
         if self.split_channel == True:
             nclasses = len(np.unique(mask))
+            split_mask = np.zeros((self.patch_size, self.patch_size, 2))
             for c in range(nclasses):
                 split_mask[:, :, c] = (mask == c).astype(int)
             split_mask = cv2.resize(split_mask,
-                                    (256, 256),
+                                    (self.patch_size, self.patch_size),
                                     interpolation=cv2.INTER_NEAREST)
-            split_mask = np.reshape(split_mask, (256*256, 2))
+            split_mask = np.reshape(split_mask,
+                                    (self.patch_size*self.patch_size, 2))
             return image, split_mask
 
         mask = mask[:, :, np.newaxis]
@@ -98,9 +98,16 @@ class InriaDataLoader(keras.utils.Sequence):
         for file in files_batch:
             image, mask = self.__load__(file)
             aug = DataAugmentation(image, mask,
-                                rotation =self.rotation,
-                                zoom_range = self.zoom_range,
-                                activate = self.aug)
+                                   rotation =self.rotation,
+                                   zoom_range = self.zoom_range,
+                                   horizontal_flip = self.horizontal_flip,
+                                   vertical_flip = self.vertical_flip,
+                                   shear = self.shear,
+                                   hist_eq = self.hist_eq,
+                                   brightness = self.brightness,
+                                   add_noise = self.add_noise,
+                                   sigma = self.sigma,
+                                   activate = self.aug)
 
             aug_images, aug_masks = aug.augment()
 
@@ -114,7 +121,6 @@ class InriaDataLoader(keras.utils.Sequence):
         masks = np.array(masks)
 
         return images, masks
-
 
     def on_epoch_end(self):
         pass
